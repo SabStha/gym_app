a184966cc3970d0b793ede73b62c25f3b3b7f901
Sabin Shrestha <sabstha98@gmail.com>
2026-02-14 18:46:37 +0900

    aa

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.2-2.858.59-4.18M5.55 17.791a4.002 4.002 0 010-6m0 0a4.002 4.002 0 01-3.765 2.247m15.462 5.068a4.002 4.002 0 01-3.83-5.263m3.83 5.263l-4.13-1.65" />
                     </svg>
-                    Sign in with Face ID
+                    Sign in with Passkey
                 </button>
                 
                 <p x-show="status" x-text="status" class="text-center text-sm text-emerald-600 font-medium"></p>
